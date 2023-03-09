58dada9a28d451064166ce18a1f2946ce81f4be8
Harish9381 <gallaharish99@gmail.com>
2023-03-09 19:24:24 +0530

    average sum program

---
+++ average_sum.py
@@ -0,0 +1,7 @@
+english = int(input("enter eng:"))
+telugu = int(input("enter tel:"))
+hindi = int(input("enter hin:"))
+sum = english + telugu + hindi
+average = (english + telugu + hindi)/3
+print(sum)
+print(average)
