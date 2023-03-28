6af821f4a67b3e07d37785abc3740b5ab75974c9
Harish9381 <gallaharish99@gmail.com>
2023-03-28 12:30:34 +0530

    done

---
+++ dic/practice.py
@@ -0,0 +1,17 @@
+p = {}
+p["name"] = "harish"
+p["id"] = 1001
+p["email"] = "harish@gmail.com"
+p["per"] = 80.76
+print(p)
+q = {}
+q["name"] = "santhosh"
+q["id"] = 1002
+q["email"] = "santhu@gmail.com"
+
+q["per"] = 81.78
+print(q)
+l = []
+l.append(p)
+l.append(q)
+print(l)
